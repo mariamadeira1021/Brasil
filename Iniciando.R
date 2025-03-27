da0d5d93f553c4403693d85199f175978f7ed55a
mariamadeira1021 <mariamlmadeira10@gmail.com>
2025-03-26 23:02:48 -0300

    Criando Repositório e Configurando R

---
+++ Iniciando.R
@@ -0,0 +1,9 @@
+# Conectando com o Git e Github
+
+install.packages("usethis")
+library(usethis)
+use_git_config(user.name = "mariamadeira1021", user.email = "mariamlmadeira10@gmail.com")
+
+# Criando Repositório
+
+use_git()
