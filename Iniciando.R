31180430591f54d95297bccf54e2412ce6702534
mariamadeira1021 <mariamlmadeira10@gmail.com>
2025-03-26 23:15:14 -0300

    Repositório Criado

--- Iniciando.R
+++ Iniciando.R
@@ -4,7 +4,14 @@ install.packages("usethis")
 library(usethis)
 use_git_config(user.name = "mariamadeira1021", user.email = "mariamlmadeira10@gmail.com")
 
-# Criando Repositório
+# Criando Repositório e Token
 
 use_git()
 library(tidyverse)
+library(usethis)
+library(gitcreds)
+create_github_token()
+gitcreds_set()
+use_github()
+
+#PRONTO!
