cb975927260be965f24e4111472501f56bc29f77
mariamadeira1021 <mariamlmadeira10@gmail.com>
2025-03-26 23:19:26 -0300

    Pacotes e Biblioteca

---
+++ Base Brasil.R
@@ -0,0 +1,10 @@
+# Instalar e carregar pacotes
+install.packages("readr")
+install.packages("dplyr")
+install.packages("DT")
+install.packages("ggplot2")
+library(readr)
+library(dplyr)
+library(DT)
+library(ggplot2)
+
