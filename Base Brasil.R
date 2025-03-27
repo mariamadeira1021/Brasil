69573b9cf0bb1bced89a7317166539c6bf9daf81
mariamadeira1021 <mariamlmadeira10@gmail.com>
2025-03-26 23:26:45 -0300

    Adicionando Ranking por total de Voto

--- Base Brasil.R
+++ Base Brasil.R
@@ -14,10 +14,16 @@ dados <- read.csv("C:/Users/maria/Downloads/base_2002_RN.csv", fileEncoding = "l
 # Criar tabela condensada rica em informação com todos os níveis
 tabela_resumo_clara <- dados %>%
   group_by(level_3, level_2, level_1, level_0,
-           turno, cargo, party_id, votes_type) %>%
+           round, office, party_id, votes_type) %>%
   summarise(total_votos = sum(QT_VOTOS, na.rm = TRUE), .groups = "drop") %>%
   
 # Adicionar percentual de votos dentro do município e turno
   group_by(level_2, round) %>%
   mutate(percentual = round(100 * total_votos / sum(total_votos), 2)) %>%
+  
+# Adicionar ranking por total de votos
+  arrange(desc(total_votos)) %>%
+  mutate(ranking = row_number()) %>%
+  
+  ungroup()
   
